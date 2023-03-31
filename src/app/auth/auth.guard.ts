8e1b25144c381054b8cb23c5c016c6d9f35660b3
Nikita Rusilko <nikita.rusilko@exposit.com>
2023-03-31 17:29:34 +0300

    split modules

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,9 +1,15 @@
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { map, take } from "rxjs/operators";
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+  UrlTree
+} from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, tap, take } from 'rxjs/operators';
 
-import { AuthService } from "./auth.service";
+import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
